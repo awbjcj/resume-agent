b09a39e732c2846e9b52a85a93b282ddc7cfdf19
awbjcj <wujiajin0303@gmail.com>
2026-06-28 16:41:42 -0400

    Change the slider tip color

--- web/src/components/ui/slider.tsx
+++ web/src/components/ui/slider.tsx
@@ -42,7 +42,7 @@ function Slider({
               key={index}
               index={index}
               aria-label={_values.length === 1 ? props["aria-label"] : undefined}
-              className="relative block size-3 shrink-0 rounded-full border border-ring bg-white ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 disabled:pointer-events-none disabled:opacity-50"
+              className="relative block size-3 shrink-0 rounded-full border border-background bg-primary ring-ring/50 transition-[color,box-shadow] select-none after:absolute after:-inset-2 hover:ring-3 focus-visible:ring-3 focus-visible:outline-hidden active:ring-3 disabled:pointer-events-none disabled:opacity-50"
             />
           ))}
         </SliderPrimitive.Track>
