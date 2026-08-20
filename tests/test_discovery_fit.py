654f0150373dc7e4152f33f37831da385343acda
awbjcj <wujiajin0303@gmail.com>
2026-08-19 20:57:16 -0400

    test: restore missing country validation case

--- tests/test_discovery_fit.py
+++ tests/test_discovery_fit.py
@@ -109,9 +109,7 @@ def test_fit_location_requires_country_but_allows_city_and_region_to_be_omitted(
     assert location.region is None
 
     with pytest.raises(ValidationError):
-        FitLocation(
-            city="Singapore", country="Singapore", region="Central"
-        )  # region is optional, but city+country is valid
+        FitLocation(city="Singapore")
 
 
 def test_score_fit_returns_location():
